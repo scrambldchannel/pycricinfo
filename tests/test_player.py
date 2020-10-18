0658ef3a8588d6dc1d104ff685e1cb42d7f58902
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-10-19 00:28:43 +0200

    to_from player tests

--- tests/test_player.py
+++ tests/test_player.py
@@ -16,25 +16,24 @@ def test_player():
     assert p.player_stats["batting"]["Tests"]["stumpings made"] == "0"
 
 
-def test_player_from_file():
+def test_player_to_from_file():
 
-    html_file = Path(__file__).parent.joinpath(
-        "serialised_objects/player/", "52812.html"
-    )
+    p = Player(52812)
+    p.to_file()
 
-    # json not available
-    json_file = None
+    p2 = Player.from_file("52812.html")
 
-    p = Player(52812, html_file=html_file, json_file=json_file)
-    assert p.player_id == 52812
-    assert len(p.html) > 0
-    assert isinstance(p.html, str)
-    assert p.name == "Sir Viv Richards"
-    assert p.full_name == "Isaac Vivian Alexander Richards"
-    assert p.batting_style == "Right-hand bat"
-    assert p.bowling_style == "Right-arm slow, Right-arm offbreak"
+    assert p2.player_id == 52812
+    assert len(p2.html) > 0
+    assert isinstance(p2.html, str)
+    assert p2.name == "Sir Viv Richards"
+    assert p2.full_name == "Isaac Vivian Alexander Richards"
+    assert p2.batting_style == "Right-hand bat"
+    assert p2.bowling_style == "Right-arm slow, Right-arm offbreak"
+    assert p2.player_stats["batting"]["Tests"]["highest inns score"] == "291"
 
-    assert p.player_stats["batting"]["Tests"]["highest inns score"] == "291"
+    p = Path("52812.html")
+    p.unlink(missing_ok=True)
 
 
 def test_player_stats():
