283dd5f6d7c8919b39bb797e8a345fe893d7cf90
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-10-19 07:01:20 +0200

    change ground

--- tests/test_ground.py
+++ tests/test_ground.py
@@ -5,14 +5,14 @@ from pycricinfo import Ground
 
 def test_ground():
     g = Ground(59269)
-    assert g.ground_id == 59269
+    assert g.id == 59269
 
 
 def test_ground_to_from_file():
     g = Ground(57129)
     g.to_file()
     g2 = Ground.from_file(html_file="57129.html")
-    g.ground_id = g2.ground_id
+    g.id = g2.id
 
     p = Path("57129.html")
     p.unlink(missing_ok=True)
