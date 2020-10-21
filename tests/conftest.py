ef05f48f7d3b863e20d53f92ad8b176f663a1874
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-10-22 00:46:27 +0200

    add basic test fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,35 @@
+import pytest
+
+
+@pytest.fixture(scope="module")
+def matches():
+    return [
+        1187009,
+        1216494,
+        1234908,
+        356789,
+        30475,
+        1152847,
+        1152783,
+        64976,
+        326203,
+        67075,
+    ]
+
+
+@pytest.fixture(scope="module")
+def players():
+    return [5600, 659081, 54962, 4188, 6467, 53477, 267192, 9130]
+
+
+@pytest.fixture(scope="module")
+def teams():
+    return [
+        30,
+        215,
+        1145839,
+        335977,
+        5153,
+        1234676,
+        233713,
+    ]
